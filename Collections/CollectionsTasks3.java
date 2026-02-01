42142671a9e4ce144be866190676ff1af9dc1388
Felix A. Taveras <felixataveras1@gmail.com>
2026-02-01 12:54:27 -0500

    saving assignment 3 - going to 4

--- Collections/CollectionsTasks3.java
+++ Collections/CollectionsTasks3.java
@@ -1,33 +1,65 @@
+
 import java.util.*;
 
 
 public class CollectionsTasks3 {
-public static void main(String[] args) {
+
+    public static void main(String[] args) {
 // TODO: Create HashMap<String, Double> for employee name and salary
 
-
-Map<String, Double> employeeSalary = new HashMap<>();
+        Map<String, Double> employeeSalary = new HashMap<>();
 
 // TODO: Add 5 employees with salaries
-
-employeeSalary.put("Ramon", 10678.90);
-employeeSalary.put("Nathan", 100678.0);
-employeeSalary.put("Valery", 1678.40);
-employeeSalary.put("Cleo", 678.99);
-employeeSalary.put("Buddy", 0.99);
-
+        employeeSalary.put("Ramon", 10678.90);
+        employeeSalary.put("Nathan", 100678.0);
+        employeeSalary.put("Valery", 1678.40);
+        employeeSalary.put("Cleo", 678.99);
+        employeeSalary.put("Buddy", 0.99);
 
 // TODO: Display all employees
-for (Map.Entry<String, Double> entry : employeeSalary.entrySet()) {
-    System.out.println(entry.getKey() + " -> " + entry.getValue());
-}
+        for (Map.Entry<String, Double> entry : employeeSalary.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+
+        }
 
 // TODO: Update salary of one employee
+        employeeSalary.replace("Buddy", 1.99);
 
 // TODO: Find employees with salary > 50000
+        employeeSalary.forEach((name, salary) -> {
+            if (salary > 50000) {
+                System.out.println(name + " -> " + salary);
+            }
+        });
 
 // TODO: Find employee with highest salary
+        final double[] maxSalary = {0};
+        LinkedHashMap<String, Double> highestSalary = new LinkedHashMap<>();
+        employeeSalary.forEach((name, salary) -> {
+            if (salary > maxSalary[0]) {
+                maxSalary[0] = salary;
+                System.out.println(name + " -> " + maxSalary[0]);
+            }
+        });
+
+
+
+
 
 // TODO: Sort HashMap by salary (descending)
-}
+
+LinkedHashMap<String, Double> descendingSalaries = employeeSalary.entrySet().stream().sorted(Map.Entry.<String, Double>comparingByValue().reversed()).collect(LinkedHashMap::new, (m, e) -> m.put(e.getKey(), e.getValue()), Map::putAll);  
+
+System.out.println(descendingSalaries);
+
+
+
+
+    }
+
+
+
+
+
+
 }
---
+++ Collections/CollectionsTasks4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+public static void main(String[] args) {
+String sentence = "Java is great and Java is powerful and Java is fun";
+
+// TODO: Split sentence into words
+
+// TODO: Create HashMap to store word frequencies
+
+// TODO: Count frequency of each word
+
+// TODO: Print each word with its count
+
+// TODO: Find the most frequent word
+}
+}
